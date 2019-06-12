e88edd65635467436166190918a09e79531f537a
Murilo Borges de Andrade <murilo.b.andrade@gmail.com>
2019-06-12 09:47:47 -0300

    remove docker and volume

--- remove_docker_redis_cluster.sh
+++ remove_docker_redis_cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker rm -f redis_0
-docker rm -f redis_1
+docker rm -v -f redis_0
+docker rm -v -f redis_1
 
-docker rm -f sentinel_0
-docker rm -f sentinel_1
-docker rm -f sentinel_2
+docker rm -v -f sentinel_0
+docker rm -v -f sentinel_1
+docker rm -v -f sentinel_2
 
 #dangerous!
 #docker volume ls | awk '{ print $2}' | xargs docker volume rm
